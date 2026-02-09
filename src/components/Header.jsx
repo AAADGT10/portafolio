74bdc6caaf9ba83fde766bcf741b85d28daa0fb6
AndresGT1022 <164397011+AndresGT1022@users.noreply.github.com>
2026-02-09 11:44:18 -0500

    hola nuevo ahora si

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,8 @@ export default function Header() {
     function handleKeydown(e) {
       if (e.key === "Escape") setIsMenuOpen(false)
     }
-// Este es un comentario de mi nuevo portafolio
+// Este es un comentario de mi nuevo portafolio y este es otro nuevo
+// buenoes6te si es nuevo
     if (isMenuOpen) {
       document.addEventListener("keydown", handleKeydown)
       document.body.style.overflow = "hidden"
